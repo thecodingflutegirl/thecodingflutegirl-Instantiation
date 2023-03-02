48749d6cce64f2b96cca22fa84355f64eb215c2e
Julia Evers <thatflutegirl12@gmail.com>
2023-03-02 17:31:30 +0000

    completed instantiation lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,9 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object newObject = new Object();
+        return newObject;
     }
+
+    
 }
